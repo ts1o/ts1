0f8d738dc81f28df7b940018cb92c787cc811f8c
ts1o <145072666+ts1o@users.noreply.github.com>
2023-09-15 07:46:29 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="ja-jp">
+
+<head>
+    <link rel="icon" href="https://drive.ts1.jp/image/favicon/shou-logo.svg" type="image/svg+xml">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/style.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/common.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/button.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/header.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/footer.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/768.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/dark_ja.css" media="screen and (prefers-color-scheme: dark)">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/style_ja.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/print-style.css" media="print">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/news.css">
+    <link rel="icon" href="https://drive.ts1.jp/image/favicon/shou-logo.svg" type="image/svg+xml">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/emg.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/faq.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/blog.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/blog_ja.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/stn.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/mourning.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/Local-Navi.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/card.css">
+    <link rel="stylesheet" href="https://drive.ts1.jp/css/wsl.css">
+    <title>TS1 Website</title>
+</head>
+
+<body>
+    <?php include('/****/****/ts1.jp/public_html/drive/html/std/header/ts1/basic.php'); ?>
+    <?php include('/****/****/ts1.jp/public_html/drive/html/std/head_menu_768/ts1/basic.php'); ?>
+    <?php include('/****/****/ts1.jp/public_html/drive/html/notice/top/basic.php'); ?>
+    <div class="main">
+        <h2>みなさまへの報告事項（令和5年9月1日公表）</h2>
+        <p>・チーム翔は、令和5年9月1日現在もその活動を停止しています。</p>
+        <p>・ts1.jpのドメイン以外のドメインの運用を終了しています。</p>
+        <p>ご理解のほど、よろしくお願いします。</p>
+        <p>チーム翔　一同</p>
+        <!-- Card -->
+        <div class="card">
+            <h2 class="card_title">発想は、止まらない。</h2>
+            <p class="card_text">発想は、止まることを知らないようです。</p>
+            <p class="card_text">新たな視点を得て、進化が始まっています。</p>
+        </div>
+        <!-- /Card -->
+        <h2>よくありそうな質問</h2>
+        <dl class="faq">
+            <dt class="faq_q">ウェブサイトのアーカイブはありますか？</dt>
+            <dd class="faq_a">はい。私たちが運営しているウェブサイト（過去に運営していたものを含む。）のアーカイブは、https://archive.org/で確認することができます。</dd>
+            <dt class="faq_q">政策はどこで見れるの？</dt>
+            <dd class="faq_a">令和5年9月1日現在、政策は公開していません。過去に公開していたものを含め、今後公開する予定です。</dd>
+        </dl>
+    </div>
+    <?php include('/****/****/ts1.jp/public_html/drive/html/std/footer/ts1/basic.php'); ?>
+</body>
+
+</html>
